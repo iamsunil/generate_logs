fb6fd5356c00c0e139f441af62fbebee93ef265a
isx8312014 <isx8312014@i20.informatica.escoladeltreball.org>
2016-05-03 09:17:07 +0200

    testing script

--- logGenerator.sh
+++ logGenerator.sh
@@ -30,14 +30,23 @@ FACILITIES=(
 	23	# local use 7  (local7)
 )
 
-levels='0 1 2 3 4 5 6 7'
+LEVELS=(
+	#0  # Emergency: system is unusable    
+	1  # Alert: action must be taken immediately
+	2  # Critical: critical conditions
+	3  # Error: error conditions
+	4  # Warning: warning conditions
+	5  # Notice: normal but significant condition
+	6  # Informational: informational messages
+	7  # Debug: debug-level messages 
+)
 
 while true
 do
 	
 	for fct in ${FACILITIES[@]}
 	do 
-		for lvl in $levels 
+		for lvl in ${LEVELS[@]} 
 		do 
 			random_message=$(pwgen 16 1)
 			logger --priority $fct.$lvl "$fct.$lvl: $random_message"
