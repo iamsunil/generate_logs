f7089f6e03b6eca370f07a03d4d4e9e27df77d5b
iamsunil <sterlingmvp1@gmail.com>
2016-05-09 10:13:12 +0200

    Update scriptmysql.py

--- scripts/scriptmysql.py
+++ scripts/scriptmysql.py
@@ -16,11 +16,11 @@ command = "mysql -h 172.17.0.2 -uroot -pjupiter insta"
 pipeData = Popen(command,shell=True,stdout=PIPE, stderr=PIPE,stdin=PIPE)
 list_tables = ["users","posts","likes","comments","followers"]
 
-while True:
-	for table in list_tables :
-		selectcmd="select * from %s;\n" % table
-		pipeData.stdin.write(selectcmd)
-		print table
+
+for table in list_tables :
+	selectcmd="select * from %s;\n" % table
+	pipeData.stdin.write(selectcmd)
+	print table
 		
 pipeData.stdin.write("\q\n")
 sys.exit(0)
