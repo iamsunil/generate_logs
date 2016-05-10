9a3c1cfe18b94ae2870102f47701b328c981054d
isx8312014 <isx8312014@localhost.localdomain>
2016-05-11 00:43:50 +0200

    adding files to metode3

---
+++ metode3/mysql_logGen.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#-*- coding: utf-8-*-
+'''
+# consulta sql
+# -----------------------------------------------------------------
+# Escola del treball de Barcelona
+# ASIX Hisi2 M06-ASO UF2NF1-Scripts
+# @edt Curs 2015-2015  Gener 2015
+# -----------------------------------------------------------------
+'''
+import sys
+from subprocess import Popen,  PIPE
+
+# Executa el pipe/subprocess
+command = "mysql --host=192.168.2.50 --port=3306  -uroot -pjupiter insta"
+pipeData = Popen(command,shell=True,stdout=PIPE, stderr=PIPE,stdin=PIPE)
+list_tables = ["users","posts","likes","comments","followers"]
+
+
+for table in list_tables :
+	selectcmd="select * from %s;\n" % table
+	pipeData.stdin.write(selectcmd)
+	print table
+		
+pipeData.stdin.write("\q\n")
+sys.exit(0)
+
